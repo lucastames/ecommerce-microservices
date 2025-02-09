eedb09fbcac48957ffb8051828c86e845f9f4231
Lucas Tames <tamess@protonmail.com>
2025-02-09 17:40:30 -0300

    feat: add initial services

---
+++ order-service/src/test/java/org/tames/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package org.tames.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
